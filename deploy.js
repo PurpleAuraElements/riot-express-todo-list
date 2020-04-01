8d52941dc4615da8054656f4ba320dc4159e3ee1
Nivia Divincenzo <nivlarae@gmail.com>
2020-04-01 15:59:54 -0400

    host fix

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.89.253.234',
+      host: '3.82.210.115',
       username: 'ubuntu',
       privateKey: 're-key.pem'
     })
